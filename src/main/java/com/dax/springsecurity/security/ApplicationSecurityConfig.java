4eda90b437bff7d379969dcc9ff9fb85a019995d
Dax Wann <daxwann@gmail.com>
2020-09-04 22:13:11 -0700

    add roles and permissions

--- src/main/java/com/dax/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/dax/springsecurity/security/ApplicationSecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.dax.springsecurity.security.ApplicationUserRole.*;
+
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -26,8 +28,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
-        .antMatchers("/", "index", "/css/*", "/js/*")
-        .permitAll()
+        .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+        .antMatchers("/api/**").hasRole(STUDENT.name())
         .anyRequest()
         .authenticated()
         .and()
@@ -40,9 +42,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     UserDetails annaUser = User.builder()
         .username("annasmith")
         .password(passwordEncoder.encode("password"))
-        .roles("STUDENT")
+        .roles(STUDENT.name())
         .build();
 
-    return new InMemoryUserDetailsManager(annaUser);
+    UserDetails lindaUser = User.builder()
+        .username("linda")
+        .password(passwordEncoder.encode("password"))
+        .roles(ADMIN.name())
+        .build();
+
+    UserDetails tomUser = User.builder()
+        .username("tom")
+        .password(passwordEncoder.encode("password"))
+        .roles(ADMIN_TRAINEE.name())
+        .build();
+
+    return new InMemoryUserDetailsManager(annaUser, lindaUser, tomUser);
   }
 }
---
+++ src/main/java/com/dax/springsecurity/security/ApplicationUserRole.java
@@ -0,0 +1,19 @@
+package com.dax.springsecurity.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.dax.springsecurity.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+  STUDENT(Sets.newHashSet()),
+  ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE)),
+  ADMIN_TRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
+
+  private final Set<ApplicationUserPermission> permissions;
+
+  ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+    this.permissions = permissions;
+  }
+}
