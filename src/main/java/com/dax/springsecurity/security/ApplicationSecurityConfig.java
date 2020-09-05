efa8435016c34998eac88c9dcce17c704f4c35c0
Dax Wann <daxwann@gmail.com>
2020-09-05 13:54:09 -0700

    configure ApplicationSecurityConfig with AuthenticationManagerBuilder

--- src/main/java/com/dax/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/dax/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,18 +1,16 @@
 package com.dax.springsecurity.security;
 
+import com.dax.springsecurity.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import java.util.concurrent.TimeUnit;
 
@@ -23,10 +21,12 @@ import static com.dax.springsecurity.security.ApplicationUserRole.*;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
   private final PasswordEncoder passwordEncoder;
+  private final ApplicationUserService applicationUserService;
 
   @Autowired
-  public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+  public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
     this.passwordEncoder = passwordEncoder;
+    this.applicationUserService = applicationUserService;
   }
 
   @Override
@@ -61,29 +61,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
   }
 
   @Override
-  @Bean
-  protected UserDetailsService userDetailsService() {
-    UserDetails annaUser = User.builder()
-        .username("annasmith")
-        .password(passwordEncoder.encode("password"))
-//        .roles(STUDENT.name()) // ROLE_STUDENT
-        .authorities(STUDENT.getGrantedAuthorities())
-        .build();
-
-    UserDetails lindaUser = User.builder()
-        .username("linda")
-        .password(passwordEncoder.encode("password"))
-//        .roles(ADMIN.name())
-        .authorities(ADMIN.getGrantedAuthorities())
-        .build();
-
-    UserDetails tomUser = User.builder()
-        .username("tom")
-        .password(passwordEncoder.encode("password"))
-//        .roles(ADMIN_TRAINEE.name())
-        .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
-        .build();
-
-    return new InMemoryUserDetailsManager(annaUser, lindaUser, tomUser);
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.authenticationProvider(daoAuthenticationProvider());
   }
+
+  @Bean
+  public DaoAuthenticationProvider daoAuthenticationProvider () {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setPasswordEncoder(passwordEncoder);
+    provider.setUserDetailsService(applicationUserService);
+
+    return provider;
+  };
 }
