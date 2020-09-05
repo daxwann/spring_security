051f9c06fc27e6eaf644eb6024a461d8bc968c96
Dax Wann <daxwann@gmail.com>
2020-09-05 16:29:59 -0700

    implement jwt authenticator and verifier

--- src/main/java/com/dax/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/dax/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.dax.springsecurity.security;
 
 import com.dax.springsecurity.auth.ApplicationUserService;
+import com.dax.springsecurity.jwt.JwtTokenVerifier;
+import com.dax.springsecurity.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +12,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.concurrent.TimeUnit;
 
 import static com.dax.springsecurity.security.ApplicationUserRole.*;
 
@@ -35,29 +36,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 //        .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 //        .and()
         .csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
+        .addFilterAfter(new JwtTokenVerifier(), JwtUsernameAndPasswordAuthenticationFilter.class)
         .authorizeRequests()
           .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
           .antMatchers("/api/**").hasRole(STUDENT.name())
           .anyRequest()
-          .authenticated()
-        .and()
-        .formLogin()
-          .loginPage("/login").permitAll()
-          .defaultSuccessUrl("/courses", true)
-          .passwordParameter("password")
-          .usernameParameter("username")
-          .and()
-          .rememberMe() // defaults to 2 weeks
-          .rememberMeParameter("remember-me")
-          .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-          .key("somethingverysecured")
-        .and()
-        .logout()
-          .logoutUrl("/logout")
-          .clearAuthentication(true)
-          .invalidateHttpSession(true)
-          .deleteCookies("JSESSIONID", "remember-me")
-          .logoutSuccessUrl("/login");
+          .authenticated();
   }
 
   @Override
